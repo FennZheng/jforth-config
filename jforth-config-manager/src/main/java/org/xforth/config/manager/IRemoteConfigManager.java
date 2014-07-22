682de46bbb419830667b34ed515ab75b1038677e
Vernon Zheng <kevonzheng@gmail.com>
2014-07-22 23:59:43 +0800

    update config-manager

--- jforth-config-manager/src/main/java/org/xforth/config/manager/IRemoteConfigManager.java
+++ jforth-config-manager/src/main/java/org/xforth/config/manager/IRemoteConfigManager.java
@@ -1,6 +1,8 @@
 package org.xforth.config.manager;
 
 public interface IRemoteConfigManager {
-    public void importProperties(String schema,String fileName);
+    public void initByProperties(String schema,String fileName) throws Exception;
+    public void update(String schema,String key,String value);
+    public void delete(String schema,String key,String value);
     public void exportProperties();
 }
--- jforth-config-manager/src/main/java/org/xforth/config/manager/RemoteConfigManager.java
+++ jforth-config-manager/src/main/java/org/xforth/config/manager/RemoteConfigManager.java
@@ -12,12 +12,14 @@ import org.springframework.beans.factory.annotation.Value;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.Enumeration;
 import java.util.Properties;
 
 public class RemoteConfigManager implements IRemoteConfigManager{
     private static final Logger logger = LoggerFactory.getLogger(RemoteConfigManager.class);
     private static final String DEFAULT_NAMESPACE = "/config-center";
+    private static final String DEFAULT_DECODE = "utf-8";
     private CuratorFramework zkClient;
     private String zkConnectString;
     public RemoteConfigManager(){
@@ -25,9 +27,17 @@ public class RemoteConfigManager implements IRemoteConfigManager{
         zkClient = CuratorFrameworkFactory.newClient(zkConnectString, retryPolicy);
         zkClient.start();
     }
+
+    /**
+     * check schema not exists
+     * @param schema
+     * @param fileName
+     * @throws Exception
+     */
     @Override
-    public void importProperties(String schema,String fileName) {
+    public void initByProperties(String schema,String fileName) throws Exception {
         String servicePath = generateServicePath(schema);
+        checkServiceConfigExists(servicePath);
         Properties prop = new Properties();
         InputStream input = null;
         try {
@@ -40,19 +50,42 @@ public class RemoteConfigManager implements IRemoteConfigManager{
         while (enums.hasMoreElements()) {
             Object key = enums.nextElement();
             prop.get(key);
-            updateRemoteConfig(servicePath,key.toString(),prop.get(key).toString());
+            importRemoteConfig(servicePath,key.toString(),prop.get(key).toString());
         }
     }
+
+    private void checkServiceConfigExists(String servicePath){
+        try {
+            if(zkClient.checkExists().forPath(servicePath)!=null){
+                throw new RuntimeException("this service config have been inited for servicePath:"+servicePath);
+            }
+        } catch (Exception e) {
+            logger.error("checkServiceConfigExists exception:{}",e);
+        }
     }
-
-    private void updateRemoteConfig(String servicePath,String key,String value){
-        zkClient.create().withMode(CreateMode.PERSISTENT).forPath(servicePath+"/local.config.remote.key3","local.config.remote.value3".getBytes("utf-8"));
-
+    private void importRemoteConfig(String servicePath,String key,String value) throws Exception {
+        final String keyPath = servicePath.concat("/").concat(key);
+        if(zkClient.checkExists().forPath(keyPath)==null){
+            zkClient.create().withMode(CreateMode.PERSISTENT).forPath(servicePath+"/"+key,value.getBytes(DEFAULT_DECODE));
+        }else{
+            throw new RuntimeException("this service config have been inited for keyPath:"+keyPath);
+        }
     }
     private String generateServicePath(String schema){
         String servicePath = DEFAULT_NAMESPACE.concat("/").concat(schema);
         return servicePath;
     }
+
+    @Override
+    public void update(String schema, String key, String value) {
+
+    }
+
+    @Override
+    public void delete(String schema, String key, String value) {
+
+    }
+
     @Override
     public void exportProperties() {
 
