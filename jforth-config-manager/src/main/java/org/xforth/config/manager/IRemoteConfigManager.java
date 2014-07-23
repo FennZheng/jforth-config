23013c24132c9846706424c0d8ffb25c603e9c51
Vernon Zheng <kevonzheng@gmail.com>
2014-07-23 11:40:07 +0800

    merge from branch vernonzheng-master

--- jforth-config-manager/src/main/java/org/xforth/config/manager/IRemoteConfigManager.java
+++ jforth-config-manager/src/main/java/org/xforth/config/manager/IRemoteConfigManager.java
@@ -1,6 +1,8 @@
 package org.xforth.config.manager;
 
 public interface IRemoteConfigManager {
-    public void importProperties(String schema,String fileName);
-    public void exportProperties();
+    public void initByProperties(String schema,String fileName) throws Exception;
+    public void update(String schema,String key,String value) throws Exception;
+    public void delete(String schema,String key,String value) throws Exception;
+    public void exportProperties(String schema) throws Exception;
 }
--- jforth-config-manager/src/main/java/org/xforth/config/manager/RemoteConfigManager.java
+++ jforth-config-manager/src/main/java/org/xforth/config/manager/RemoteConfigManager.java
@@ -8,16 +8,19 @@ import org.apache.zookeeper.CreateMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Properties;
-
+@Component
 public class RemoteConfigManager implements IRemoteConfigManager{
     private static final Logger logger = LoggerFactory.getLogger(RemoteConfigManager.class);
     private static final String DEFAULT_NAMESPACE = "/config-center";
+    private static final String DEFAULT_DECODE = "utf-8";
     private CuratorFramework zkClient;
     private String zkConnectString;
     public RemoteConfigManager(){
@@ -25,9 +28,17 @@ public class RemoteConfigManager implements IRemoteConfigManager{
         zkClient = CuratorFrameworkFactory.newClient(zkConnectString, retryPolicy);
         zkClient.start();
     }
+
+    /**
+     * init by properties file
+     * @param schema
+     * @param fileName
+     * @throws Exception :if schema exists,throw RuntimeException
+     */
     @Override
-    public void importProperties(String schema,String fileName) {
+    public void initByProperties(String schema,String fileName) throws Exception {
         String servicePath = generateServicePath(schema);
+        checkServiceConfigExists(servicePath);
         Properties prop = new Properties();
         InputStream input = null;
         try {
@@ -40,22 +51,61 @@ public class RemoteConfigManager implements IRemoteConfigManager{
         while (enums.hasMoreElements()) {
             Object key = enums.nextElement();
             prop.get(key);
-            updateRemoteConfig(servicePath,key.toString(),prop.get(key).toString());
+            importRemoteConfig(servicePath, key.toString(), prop.get(key).toString());
         }
     }
+
+    private void checkServiceConfigExists(String servicePath){
+        try {
+            if(zkClient.checkExists().forPath(servicePath)!=null){
+                throw new RuntimeException("this service config have been inited for servicePath:"+servicePath);
+            }
+        } catch (Exception e) {
+            logger.error("checkServiceConfigExists exception:{}",e);
+        }
     }
-
-    private void updateRemoteConfig(String servicePath,String key,String value){
-        zkClient.create().withMode(CreateMode.PERSISTENT).forPath(servicePath+"/local.config.remote.key3","local.config.remote.value3".getBytes("utf-8"));
-
+    private void importRemoteConfig(String servicePath,String key,String value) throws Exception {
+        final String keyPath = servicePath.concat("/").concat(key);
+        if(zkClient.checkExists().forPath(keyPath)==null){
+            zkClient.create().withMode(CreateMode.PERSISTENT).forPath(servicePath+"/"+key,value.getBytes(DEFAULT_DECODE));
+        }else{
+            throw new RuntimeException("this service config have been inited for keyPath:"+keyPath);
+        }
     }
     private String generateServicePath(String schema){
-        String servicePath = DEFAULT_NAMESPACE.concat("/").concat(schema);
-        return servicePath;
+        return DEFAULT_NAMESPACE.concat("/").concat(schema);
     }
-    @Override
-    public void exportProperties() {
 
+    private String generateKeyPath(String schema,String key){
+        return generateServicePath(schema).concat("/").concat(key);
+    }
+
+    @Override
+    public void update(String schema, String key, String value) throws Exception {
+        final String keyPath = generateKeyPath(schema,key);
+        zkClient.setData().forPath(keyPath, value.getBytes(DEFAULT_DECODE));
+    }
+
+    @Override
+    public void delete(String schema, String key, String value) throws Exception {
+        final String keyPath = generateKeyPath(schema,key);
+        zkClient.delete().forPath(keyPath);
+    }
+
+    @Override
+    public void exportProperties(String schema) throws Exception {
+        final String servicePath = generateServicePath(schema);
+        HashMap<String,String> propertyMap = new HashMap<String, String>();
+        if(zkClient.checkExists().forPath(servicePath)==null) {
+            List<String> keyPathList = zkClient.getChildren().forPath(servicePath);
+            if(keyPathList!=null){
+                for(String keyPath:keyPathList){
+                    byte[] data = zkClient.getData().forPath(keyPath);
+                    String key = keyPath.replaceAll(".*//", "");
+                    propertyMap.put(key,new String(data,DEFAULT_DECODE));
+                }
+            }
+        }
     }
 
     public String getZkConnectString() {
